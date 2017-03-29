517fb60be2d7cd0f4e539d2b4d9f3da4b13a5f83
ktsn <ktsn55@gmail.com>
2017-03-29 15:26:02 +0900

    Add accordion component

---
+++ src/browser.js
@@ -0,0 +1,3 @@
+export const raf = window.requestAnimationFrame.bind(window)
+
+export const getStyle = window.getComputedStyle
--- test/all.test.js
+++ test/all.test.js
@@ -1,3 +1,5 @@
 import RadioTest from './RadioTest.html'
+import AccordionTest from './AccordionTest.html'
 
 new RadioTest({ target: document.getElementById('radio') })
+new AccordionTest({ target: document.getElementById('accordion') })
