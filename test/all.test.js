5265efdc07c3a79b5a93c873c1a37abf608bbb76
ktsn <ktsn55@gmail.com>
2017-04-02 22:38:49 +0900

    Add tree

--- test/all.test.js
+++ test/all.test.js
@@ -1,7 +1,9 @@
 import RadioTest from './RadioTest.html'
 import AccordionTest from './AccordionTest.html'
 import ModalTest from './ModalTest.html'
+import TreeTest from './TreeTest.html'
 
 new RadioTest({ target: document.getElementById('radio') })
 new AccordionTest({ target: document.getElementById('accordion') })
 new ModalTest({ target: document.getElementById('modal') })
+new TreeTest({ target: document.getElementById('tree') })
