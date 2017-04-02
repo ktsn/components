a6f04e30c5aab17670ff871313fb38649f34fd77
ktsn <ktsn55@gmail.com>
2017-04-02 22:18:06 +0900

    Add modal component

--- src/browser.js
+++ src/browser.js
@@ -1,3 +1,7 @@
 export const raf = window.requestAnimationFrame.bind(window)
 
 export const getStyle = window.getComputedStyle
+
+export function append (parent, child) {
+  parent.appendChild(child)
+}
--- test/all.test.js
+++ test/all.test.js
@@ -1,5 +1,7 @@
 import RadioTest from './RadioTest.html'
 import AccordionTest from './AccordionTest.html'
+import ModalTest from './ModalTest.html'
 
 new RadioTest({ target: document.getElementById('radio') })
 new AccordionTest({ target: document.getElementById('accordion') })
+new ModalTest({ target: document.getElementById('modal') })
